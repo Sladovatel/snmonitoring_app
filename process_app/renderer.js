54ddaa2c7257b789422fe81e7071d741f7f6753d
Slaovatel <167723400+Sladovatel@users.noreply.github.com>
2025-10-12 17:24:46 +0300

    Clearing...

--- process_app/renderer.js
+++
@@ -1,2 +0,0 @@
-const information = document.getElementById('info')
-information.innerText = `Приложение использует Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), и Electron (v${versions.electron()})`
